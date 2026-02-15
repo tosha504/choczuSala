8c2ffb87f6b6e443ccf0212dd452945bc099253c
tosha504 <tosha.rovniy@gmail.com>
2026-02-15 18:34:01 +0100

    remove cart

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -77,20 +77,19 @@ defined('ABSPATH') || exit;
 
                     <td class="wrap">
                         <div class="product-remove">
-                            <?php
-                            echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                                'woocommerce_cart_item_remove_link',
-                                sprintf(
-                                    '<a ... data-product_id="%s" data-product_sku="%s">&times;</a>',
-                                    esc_url(wc_get_cart_remove_url($cart_item_key)),
-                                    esc_html__('Remove this item', 'woocommerce'),
-                                    esc_attr($_product->get_id()),
-                                    esc_attr($cart_item_key),
-                                    esc_attr($_product->get_sku()),
-                                ),
-                                $cart_item_key
-                            );
-                            ?>
+<?php
+	echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		'woocommerce_cart_item_remove_link',
+		sprintf(
+			'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+			esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+			esc_attr__( 'Remove this item', 'woocommerce' ),
+			esc_attr( $_product->get_id() ),
+			esc_attr( $_product->get_sku() )
+		),
+		$cart_item_key
+	);
+	?>
                         </div>
 
                         <div class="product-total">
